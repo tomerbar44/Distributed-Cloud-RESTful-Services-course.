cef67faafcaf2574f77d9961c58402863308b8b2
tomerbar44 <tomebar44@gmail.com>
2019-05-24 14:14:21 +0300

    update

---
+++ exercises/firstLevel/includes/get_params_login.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Tomer Bar</title>
+    <link href="https://fonts.googleapis.com/css?family=Baloo+Bhai&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body id="detailsSaveLayout">
+    <div id="wrapper">
+        <header>
+            <section class="loginLine">
+                <a href="#">Efrat</a>
+                <a href="#">Support</a>
+            </section>
+            <section class="logoSection">
+                <a class="logo1" href="#"></a>
+                <a class="logo2" href="#"></a>
+            </section>
+
+        </header>
+
+
+        <nav>
+            <ul class="nav nav-tabs">
+                <li class="nav-item">
+                    <a class="nav-link" href="../index.html">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Report</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="../facilitysLayout.html">Facilities</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Subscribers</a>
+                </li>
+            </ul>
+        </nav>
+
+        <ul class="breadcrumb">
+            <li><a href="../index.html">Home</a></li>
+            <li><a href="../facilitysLayout.html">Facilities</a></li>
+            <li><a href="../formLayout.html">Add a facility</a></li>
+            <li class="currentPage">Details saved</li>
+        </ul>
+
+        <main>
+        <?php
+        $uname=$_GET["name"];
+        $ucode=$_GET["code"];
+        $unum=$_GET["number"];
+        $upro=$_GET["provider"];
+        $udate=$_GET["date"];
+        $ucomm=$_GET["comments"];
+
+            echo "<table class=table>
+                <thead>
+                    <tr>
+                        <th scope=col></th>
+                        <th scope=col>Name</th>
+                        <th scope=col>Code</th>
+                        <th scope=col>Number</th>
+                        <th scope=col>Provider</th>
+                        <th scope=col>Date</th>
+                        <th scope=col>Comments</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope=row>1</th>
+                        <td>". $uname ."</td>
+                        <td>". $ucode ."</td>
+                        <td>". $unum ."</td>
+                        <td>". $upro ."</td>
+                        <td>". $udate ."</td>
+                        <td>". $ucomm ."</td>
+
+                    </tr>
+
+                </tbody>
+            </table>";
+
+        ?>
+        </main>
+
+
+    </div>
+    
+</body>
+
+</html>
--- exercises/firstLevel/includes/main.js
+++ exercises/firstLevel/includes/main.js
@@ -1,62 +1,28 @@
 //Global variables
-var g_nboxHeight = 80,
-    g_nboxWidth = 80,
-    arrBox = [],
-    g_i;
-const arrChar = ['T', 'O', 'M', 'E', 'R', 'B', 'A'];
 
 (function() {
+    var filter;
 
-    //A memory game begins after clicking a box
-    function flipBox() {
-
-        if (this.style.color === 'black') {
-            this.style.color = 'white';
-            arrBox.push(this);
-        } else {
-            this.style.color = 'black';
-            arrBox.pop(this);
-        }
-        if (arrBox.length == 2) {
-            if (arrBox[0].innerHTML === arrBox[1].innerHTML) {
-                arrBox[0].style.background = 'red';
-                arrBox[1].style.background = 'red';
-                arrBox[0].removeEventListener('click', flipBox);
-                arrBox[1].removeEventListener('click', flipBox);
+    function categoryFiltering() {
+        if (this.innerHTML === 'Power') {
+            filter = document.getElementsByClassName('power');
+            if (filter[0].style.display === 'none') {
+                filter[0].style.display = 'block';
             } else {
-                let box1 = arrBox[0];
-                let box2 = arrBox[1];
-                setTimeout(() => {
-                    box1.style.color = 'black';
-                    box2.style.color = 'black';
-                }, 700)
+                filter[0].style.display = 'none';
             }
-            arrBox = [];
+        } else {
+            filter = document.getElementsByClassName('aerobic');
+            if (filter[0].style.display === 'none') {
+                filter[0].style.display = 'block';
+            } else {
+                filter[0].style.display = 'none';
+            }
         }
+
     }
 
-    // Create three boxes with random letters each pressing the button 
-    function addFloatBox() {
-        for (g_i = 0; g_i < 3; ++g_i, g_nboxHeight += 20, g_nboxWidth += 20) {
-            const main = document.getElementsByTagName('main')[0];
-            let box = document.createElement('div');
-            box.className = 'floatBox';
-            box.style.width = g_nboxWidth + 'px';
-            box.style.height = g_nboxHeight + 'px';
-            box.style.color = 'black';
-            box.style.fontSize = (g_nboxHeight - 10) + 'px';
-            box.style.textAlign = 'center';
-            box.style.backgroundColor = 'black';
-            box.style.float = 'left';
-            box.style.margin = 64 + 'px';
-            let random = Math.floor((Math.random() * 100)) % arrChar.length;
-            let letter = arrChar[random];
-            box.innerHTML = letter;
-            box.addEventListener('click', flipBox);
-            main.append(box);
-        }
-    }
-
-    document.getElementById('button').onclick = addFloatBox;
+    document.getElementById('filterPower').onclick = categoryFiltering;
+    document.getElementById('filterAerobic').onclick = categoryFiltering;
 
 })();
