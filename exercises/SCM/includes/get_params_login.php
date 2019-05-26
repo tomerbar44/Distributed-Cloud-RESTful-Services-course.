12510dc3f7ba83074b46351fca652ab2cb37c61a
tomerbar44 <tomebar44@gmail.com>
2019-05-26 19:32:52 +0300

    update css and js

--- exercises/SCM/includes/get_params_login.php
+++ exercises/SCM/includes/get_params_login.php
@@ -2,8 +2,7 @@
 <html>
 
 <head>
-    <title>Tomer Bar</title>
-    <link href="https://fonts.googleapis.com/css?family=Baloo+Bhai&display=swap" rel="stylesheet">
+    <title>SCM</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
 </head>
@@ -15,10 +14,8 @@
                 <a href="#">Efrat</a>
                 <a href="#">Support</a>
             </section>
-            <section class="logoSection">
-                <a class="logo1" href="#"></a>
-                <a class="logo2" href="#"></a>
-            </section>
+            <a class="logo1" href="#"></a>
+            <a class="logo2" href="#"></a>
         </header>
         <nav>
             <ul class="nav nav-tabs">
--- exercises/SCM/includes/main.js
+++ exercises/SCM/includes/main.js
@@ -1,17 +1,18 @@
 (function() {
     function categoryFiltering() {
-        var powerGroup, aerobicGroup;
+        var powerGroup = [],
+            aerobicGroup = [];
         powerGroup = document.getElementsByClassName('power');
         aerobicGroup = document.getElementsByClassName('aerobic');
         if (this.innerHTML === 'Aerobic') {
-            if (powerGroup[0].style.display === 'block') // for two clicks
+            if (powerGroup[0].style.display === 'block') // check two clicks
                 powerGroup[0].style.display = 'none';
             else {
                 powerGroup[0].style.display = 'block';
             }
             aerobicGroup[0].style.display = 'block';
         } else {
-            if (aerobicGroup[0].style.display === 'block') // for two clicks
+            if (aerobicGroup[0].style.display === 'block') // check two clicks
                 aerobicGroup[0].style.display = 'none';
             else {
                 aerobicGroup[0].style.display = 'block';
@@ -20,7 +21,7 @@
 
         }
     }
-
+    // triggers
     document.getElementById('filterPower').onclick = categoryFiltering;
     document.getElementById('filterAerobic').onclick = categoryFiltering;
 
