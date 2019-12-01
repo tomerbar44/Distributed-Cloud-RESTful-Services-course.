f98049e8b49388725c8a871f856fd6f9630e7762
tomerbar44 <tomebar44@gmail.com>
2019-12-01 18:49:34 +0200

    Update index.js

--- exercises/ex_2_node/index.js
+++ exercises/ex_2_node/index.js
@@ -18,19 +18,48 @@ const server = http.createServer((req, res) => {
         req.on("data", data => {
             body += data;
         });
-        req.on("end", () => {
-            body = JSON.parse(body);
-            const newInvite = {
-                moment: new Date(),
-                id: "id-" + Date.now(),
-                user: body.user,
-                cards: body.cards
-            };
-            invites[newInvite.id] = newInvite;
-            cards += newInvite.cards;
-            console.log("invites", invites);
-            res.end(JSON.stringify(newInvite));
-        });
+        if (url.parse(req.url, true).pathname === "/addInvite") {
+            req.on("end", () => {
+                body = JSON.parse(body);
+                const newInvite = {
+                    moment: new Date(),
+                    id: Date.now(),
+                    user: body.user,
+                    cards: body.cards
+                };
+                invites[newInvite.id] = newInvite;
+                cards -= newInvite.cards;
+                console.log("invites", invites);
+                res.end(JSON.stringify(newInvite));
+            });
+        }
+        // change methot to delete
+        if (url.parse(req.url, true).pathname === "/deleteInvite") {
+            req.on("end", () => {
+                body = JSON.parse(body);
+                console.log("id:", body.id);
+                console.log("this cards", invites[body.id].cards);
+                cards += invites[body.id].cards;
+                delete invites[body.id];
+                res.end("Invitation deleted");
+                console.log("invites", invites);
+                console.log("cards", cards);
+            });
+        }
+        // change methot to put
+        if (url.parse(req.url, true).pathname === "/updateInvite") {
+            req.on("end", () => {
+                body = JSON.parse(body);
+                console.log("id:", url.parse(req.url, true).query.id);
+
+                res.end("Invitation deleted");
+                console.log("invites", invites);
+                console.log("cards", cards);
+            });
+        }
+
+
+
     }
 });
 
